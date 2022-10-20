f4feabf93d7c9b0e5d1bc26a00e0439967d28e85
miguelcram <miguel.cabralramirez@alum.uca.es>
2022-10-20 19:36:03 +0200

    Todo preparado ahora

--- p1/DefenseStrategy.cpp
+++ p1/DefenseStrategy.cpp
@@ -17,7 +17,6 @@
 RAND_TYPE SimpleRandomGenerator::a;
 #endif
 
-// CAMBIOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOS PRUEBA
 using namespace Asedio;
 
 // Devuelve la posición en el mapa del centro de la celda (i,j)
